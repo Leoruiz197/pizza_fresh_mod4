407370f7a00c86818a90d185c5b112af0fa970bf
Leo Ruiz <ruizruizleo@hotmail.com>
2022-06-10 21:49:32 -0300

    compodoc completo

---
+++ src/prisma/seed/01-user.ts
@@ -0,0 +1,30 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+export const users: Prisma.UserCreateInput[] = [
+  {
+    nickname: 'paulosalvatore',
+    name: 'Paulo Salvatore',
+    password: 'Abcd@1234',
+    image: 'https://avatars.githubusercontent.com/u/7906171',
+  },
+  {
+    nickname: 'leoruiz197',
+    name: 'Leonardo Ruiz',
+    password: 'Blue2022123',
+    image: 'https://avatars.githubusercontent.com/u/7906171',
+  },
+];
+
+export const user = async (prisma: PrismaClient) => {
+  for (const obj of Object.values(users)) {
+    await prisma.user.upsert({
+      where: { nickname: obj.nickname },
+      update: {},
+      create: {
+        ...obj,
+        password: await bcrypt.hash(obj.password, 10),
+      },
+    });
+  }
+};
